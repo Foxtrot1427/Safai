c959dd90c28dbf3bd41d2417df9d5edff8201681
Ishan Adhikari <foxtrot@Ishans-MBP.netis.cc>
2024-08-22 11:25:50 +0545

    fix: donation user side fix

--- src/pages/what-we-buy/donation/constant.ts
+++ src/pages/what-we-buy/donation/constant.ts
@@ -4,11 +4,27 @@ export const schema = yup.object().shape({
   name: yup.string().required("Name is required"),
   image: yup.mixed<FileList>(),
   itemName: yup.string().required("Item name is required"),
-  pickUpDate: yup.string(),
   pickUpTime: yup.string(),
+  pickUpDate: yup.string(),
   phoneNumber: yup.string().required("Phone number is required"),
-  description: yup.string().required("Description is required"),
+  description: yup.string(),
+}).test('pickUpDate-and-time', 'Both pick up date and time are required when either is provided', function (values) {
+  const { pickUpDate, pickUpTime } = values;
+  if ((pickUpDate && !pickUpTime)) {
+    return this.createError({
+      path: 'pickUpTime',
+      message: 'Pick up time is required when Date is provided',
+    });
+  }
+  else if ((!pickUpDate && pickUpTime)) {
+    return this.createError({
+      path: 'pickUpDate',
+      message: 'Pick up date is required when Time is provided',
+    });
+  }
+  return true;
 });
+
 
 export type DonationFormValues = yup.InferType<typeof schema>;
 
